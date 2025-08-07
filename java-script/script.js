d4c7fb0b1e4b2b3f72f2ef1eb8281cc8bdba770c
tanzeela <tanzeelafarooq-st@outlook.com>
2025-08-07 09:56:38 +0530

    add if statment

--- java-script/script.js
+++ java-script/script.js
@@ -54,11 +54,26 @@ switch (true){
     default : 
         console.log("fail");
 }
-// let age=21;
-// let isStudent=true;
-// let isWeekend=false;
-// let ticketPrice;
-//  if (age<5){
-//     ticketPrice=0;
+let age=21;
+ let isStudent=true;
+ let isWeekend=false;
+let ticketPrice;
+  if (age < 5){
+     ticketPrice=0;
     
-//  }
+    }else{
+      if (isWeekend){
+         ticketPrice = 300;
+      } else{
+          ticketPrice =200;
+  }
+  //apply discounts
+    if (age >60 ){
+    ticketPrice = ticketPrice*0.7;  //senior discount =30% off
+    } else if (isStudent && !isWeekened){
+       ticketPrice =ticketPrice*0.5;   //student discount only on weekdays =50% off
+    }
+
+    
+ }
+ console.log ("final ticket price :" ,ticketPrice)
