77fa36bcbbb9ae3297a61a14f93d910a82c2ec84
tanzeela <tanzeelafarooq-st@outlook.com>
2025-08-08 21:02:01 +0530

    add ticketprice task

--- java-script/script.js
+++ java-script/script.js
@@ -1,27 +1,28 @@
            // switch case
-//  let marks= Number(prompt("Enter the number"));
-// console.log (marks)
-// switch (true){
-//     case  (marks >=90 && marks <=100):
-//           console.log("grade A");
-//           console.log("Excellent!")
-//         break;
-//      case  (marks >=80 && marks <90):
-//           console.log("grade B");
-//            console.log("Very Good!")
-//         break;   
-//     case  (marks >=70  && marks <80):
-//           console.log("grade C");
-//           console.log("Good!")
-//            break;   
-//      case  (marks >=60 && marks < 70):
-//           console.log("grade D");
-//           console.log("passed")
-//         break; 
+           console.log ("switch case");
+  let marks= Number(prompt("Enter the percentage"));
+console.log (marks)
+switch (true){
+    case  (marks >=90 && marks <=100):
+          console.log("grade A");
+          console.log("Excellent!")
+        break;
+     case  (marks >=80 && marks <90):
+          console.log("grade B");
+           console.log("Very Good!")
+        break;   
+    case  (marks >=70  && marks <80):
+          console.log("grade C");
+          console.log("Good!")
+           break;   
+     case  (marks >=60 && marks < 70):
+          console.log("grade D");
+          console.log("passed")
+        break; 
    
-//     default : 
-//         console.log("fail");
-// }
+    default : 
+        console.log("fail");
+}
       //TASK 
  let age=prompt("enter your age");
  let input1=prompt("are you a student")
@@ -39,52 +40,26 @@
    isWeekend=false;
  }
 
-// let ticketPrice;
-//   if (age <= 5){
-//      ticketPrice=0;
+let ticketPrice;
+  if (age <= 5){
+     ticketPrice=0;
     
-//     }else{
-//       if (isWeekend){
-//          ticketPrice = 300;
-//       } else{
-//           ticketPrice =200;
-//   }
-//   //apply discounts
-//     if (age >=60 ){
-//     ticketPrice = ticketPrice*0.7;  //senior discount =30% off
-//     } else if (isStudent && !isWeekend){
-//        ticketPrice =ticketPrice*0.5;   //student discount only on weekdays =50% off
-//     }
+    }else{
+      if (isWeekend){
+         ticketPrice = 300;
+      } else{
+          ticketPrice =200;
+  }
+  //apply discounts
+    if (age >=60 ){
+    ticketPrice = ticketPrice*0.7;  //senior discount =30% off
+    } else if (isStudent && !isWeekend){
+       ticketPrice =ticketPrice*0.5;   //student discount only on weekdays =50% off
+    }
     
 
     
-//  }
-//  console.log ("final ticket price :" ,ticketPrice)
-
-//  //switch case
-//    let grade ="A";
-//    switch(grade){
-//       case "A":
-//       console.log("Excellent");
-//       break;
-//       case "B":
-//       console.log("Very good");
-//       break;
-      
-//       case "C":
-//       console.log("Good");
-//       break;
-//       default:
-//          console.log("fail");
-      
-
-//    }
-
- function info( age, isStudent, isWeekend){
-   console.log=("age");
-   console.log=("isWeekend");
-   return isStudent
-   
  }
- info(21 ,true ,false);
- CO
+ console.log ("final ticket price :" ,ticketPrice)
+
+
